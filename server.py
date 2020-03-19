fbb0292cf029d0d60e5a75db8c247164c59731e1
Orango Mango <61402409+OrangoMango@users.noreply.github.com>
2020-03-19 19:48:57 +0100

    server.py file

---
+++ server.py
@@ -0,0 +1,69 @@
+import socket, os, sys
+from threading import Thread
+
+HOST = os.popen('ifconfig | grep "inet addr" | cut -d: -f2 | cut -d" " -f1 | head -1').read().strip() #socket.gethostname()
+
+RESET = "\033[0;0m"
+BOLD  = "\033[;1m"
+RED  = "\033[91m"
+GREEN = "\033[92m"
+
+print("Host: {0}".format(HOST))
+PORT = input("Port: ")
+BUFSIZ = 1024
+ADDR = (HOST, int(PORT))
+
+SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+SERVER.bind(ADDR)
+
+SERVER.listen(1)
+
+def from_client(client, adress):
+	while True:
+		try:
+			msg = client.recv(BUFSIZ).decode("utf-8")
+			#print(msg)
+			if msg == "LEFT":
+				leftname = people[adress[1]]
+				sys.stdout.write(BOLD+RED+people[adress[1]]+" left"+RESET+"\n")
+				names.remove(people[adress[1]])
+				del people[adress[1]]
+				sks.remove(client)
+				client.sendall(bytes("left", "utf-8"))
+				print("2", names, people)
+				for sock in sks:
+					if sock == client:
+						continue
+					sock.send(bytes("INFO: "+BOLD+RED+leftname+" left"+RESET+"\n", "utf-8"))
+				return
+				#continue
+			sys.stdout.write(people[adress[1]]+"> "+msg)
+			for sock in sks:
+				if sock == client:
+					continue
+				sock.send(bytes(people[adress[1]]+": "+msg, "utf-8"))
+		except Exception as e:
+			print("While loop exited [{}]".format(e))
+			#client.close()
+			break
+
+people = {}
+names = []
+sks = []
+
+print("Waiting for connections...")
+while True:
+	c, a = SERVER.accept()
+	sks.append(c)
+	name = c.recv(BUFSIZ).decode("utf-8")
+	#print("NAME", name)
+	sys.stdout.write(BOLD+GREEN+"Connected "+a[0]+":"+str(a[1])+RESET+"\n")
+	people[a[1]] = name
+	names.append(name)
+	for sock in sks:
+		if sock == c:
+			continue
+		sock.send(bytes("INFO: "+BOLD+GREEN+"Connected "+a[0]+":"+str(a[1])+" ("+"%s" % (name) +")"+RESET+"\n", "utf-8"))
+	Thread(target=from_client, args=(c,a)).start()
+	print(names, people)
+	#c.send(bytes("Welcome!", "utf-8"))
