44354358a679b598e34c414fe0103672ca2e1eac
Orango Mango <61402409+OrangoMango@users.noreply.github.com>
2020-03-24 20:02:19 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,17 +1,20 @@
 import socket, os, sys
 from threading import Thread
 
-HOST = os.popen('ifconfig | grep "inet addr" | cut -d: -f2 | cut -d" " -f1 | head -1').read().strip() #socket.gethostname()
+try:
+	HOST, PORT = sys.argv[1], int(sys.argv[2])
+except:
+	print("Invalid arguments given:")
+	HOST = input("Enter host: ")
+	PORT = input("Port: ")
+
+BUFSIZ = 1024
+ADDR = (HOST, int(PORT))
 
 RESET = "\033[0;0m"
 BOLD  = "\033[;1m"
 RED  = "\033[91m"
 GREEN = "\033[92m"
-
-print("Host: {0}".format(HOST))
-PORT = input("Port: ")
-BUFSIZ = 1024
-ADDR = (HOST, int(PORT))
 
 SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 SERVER.bind(ADDR)
@@ -19,6 +22,8 @@ SERVER.bind(ADDR)
 SERVER.listen(1)
 
 #functon that converts a list into a string with separator: ';'
+#['hi', 'my', 'name', 'is'] becomes hi;my;name;is
+
 def convert_list(lista):
 	string = ""
 	for item in lista:
@@ -35,14 +40,12 @@ def from_client(client, adress):
 	while True:
 		try:
 			msg = client.recv(BUFSIZ).decode("utf-8")
-			#print(msg)
 			if msg == "LEFT":
 				leftname = people[adress[1]]
 				sys.stdout.write(BOLD+RED+people[adress[1]]+" left"+RESET+"\n")
 				names.remove(people[adress[1]])
 				del people[adress[1]]
 				sks.remove(client)
-				#print("2", names, people)
 				for sock in sks:
 					if sock == client:
 						continue
@@ -57,28 +60,29 @@ def from_client(client, adress):
 				if sock == client:
 					continue
 				sock.send(bytes(people[adress[1]]+": "+msg, "utf-8"))
+
 		except Exception as e:
-			print("While loop exited [{}]".format(e))
+			print("While loop exit because of {}".format(e)) #To remove
 			client.close()
 			break
 
-people = {}
-names = []
-sks = []
+people = {} #dictionary of PIDs and names
+names = []  #list of names
+sks = []    #list of sockets
+
+print("#"+"-"*30+"#") #separator
 
 print("Waiting for connections...")
 while True:
 	c, a = SERVER.accept()
 	sks.append(c)
 	name = c.recv(BUFSIZ).decode("utf-8")
-	#print("NAME", name)
-	sys.stdout.write(BOLD+GREEN+"Connected "+a[0]+":"+str(a[1])+RESET+"\n")
+	sys.stdout.write(BOLD+GREEN+a[0]+":"+str(a[1])+" connected"+RESET+"\n")
 	people[a[1]] = name
 	names.append(name)
 	for sock in sks:
 		if sock == c:
 			continue
-		sock.send(bytes("INFO: "+BOLD+GREEN+"Connected "+a[0]+":"+str(a[1])+" ("+"%s" % (name) +")"+RESET+"\n", "utf-8"))
+		sock.send(bytes("INFO: "+BOLD+GREEN+a[0]+":"+str(a[1])+" ("+"%s" % (name) +")"+" joined"+RESET+"\n", "utf-8"))
 	Thread(target=from_client, args=(c,a)).start()
 	#print(names, people)
-	#c.send(bytes("Welcome!", "utf-8"))
